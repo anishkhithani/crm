3215b2462bdf31560b261d2b9dbaecbe04b755ad
anishkhithani <anish.khithani@gmail.com>
2013-06-10 18:57:26 -0400

    finished contact less except for display all, thinking about best way to show this

--- contact_test.rb
+++ contact_test.rb
@@ -1,15 +1,28 @@
 require 'test/unit'
 
-require_relative ../contact
+require_relative './contact'
 
-class TestConact < Test::Unit::TestCase
+class TestContact < Test::Unit::TestCase
 
-    def initialize_with_valid_params_works
-        contact = Contact.new(5,"Will","Richman","will@bitmakerlabs.com", "notes")
+    def setup
+        @entry = Contact.new(5,"Will","Richman","will@bitmakerlabs.com", "notes")
+    end
+
+
+    def test_initialize_with_valid_params_works
         #assert that the variables passed in are retrievable and therefore stored
         #assert that you get back an instance of a contact
-        assert contact.is_a?(Contact)
-        assert_equal 5, contact.id
+        # assert(entry.is_a?(Contact))
+        assert_equal(5, @entry.id)
+        assert_equal("Will", @entry.firstname)
+        assert_equal("Richman", @entry.lastname)
+        assert_equal("will@bitmakerlabs.com", @entry.email)
+        assert_equal("notes", @entry.notes)
+    end
+
+    def test_display_all_entries_onscreen
+        #think about how to test this function aside from copy+paste
+        #an entire block of text...
     end
 
 end
